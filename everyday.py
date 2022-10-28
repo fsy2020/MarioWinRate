3fcbfd039d555d6d69270f7592cf63b6fc162c7c
fsy2020 <fsy781653822@gmail.com>
2022-10-28 16:39:28 +0900

    bug fixed

--- everyday.py
+++ everyday.py
@@ -10,7 +10,7 @@ class API():
         self.user_id = user_id
 
     def read_lastline(self,user_id):
-        with open( name + ".csv") as csvfile:
+        with open( pid + ".csv") as csvfile:
             mlines = csvfile.readlines()
             targetline = mlines[-1]
             list = targetline.split(',')
@@ -25,6 +25,7 @@ class API():
         res = requests.get(url)
         time.sleep(1)
         #放在while循环中，防止json解析报错
+
         if(res.status_code==200):
             return json.loads(res.text)
         else:
@@ -49,7 +50,7 @@ class API():
 
 
     def write_to_csv(self,user_id):
-        with open( name + ".csv", 'a+', newline='') as csvfile:
+        with open( pid + ".csv", 'a+', newline='') as csvfile:
             win, plays, rate, rate_change, win_total, plays_total, versus_rating = api.get_print(user_id)
             writer = csv.writer(csvfile)
             writer.writerow([win, plays, versus_rating, rate ,rate_change, win_total, plays_total,time_n])
@@ -64,7 +65,7 @@ class API():
 
 
     def create_csv(self,user_id):
-        with open( name + ".csv", "w",newline='') as csvfile:
+        with open( pid + ".csv", "w",newline='') as csvfile:
             writer = csv.writer(csvfile)
             writer.writerow(["wins","plays","win_rate","rate","rate_change","wins_total","plays_total","time"])
             rate, win_total, plays_total= api.first_line(user_id)
@@ -74,22 +75,24 @@ class API():
 if __name__ == '__main__':
     start = time.time()
     time_n = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
-    user_id = "Y9P7BN4JF"
-    user_ids = ["Y9P7BN4JF", "0MMCG9V4G", "SQW0796SF", "GDH8R4V4G", "D221SPHLF","Q5MBL99QG","D049HCB8G","1VVRCXQPF","D8CJ2W62H","LDMLC6RLG","0JR5R5BJG","4QVF9V6RF","08VW66RLF"]
+    user_id = "0MMCG9V4G"
+    user_ids = ["Y9P7BN4JF", "SQW0796SF", "GDH8R4V4G", "D221SPHLF","Q5MBL99QG","4QVF9V6RF","D049HCB8G","1VVRCXQPF","D8CJ2W62H","LDMLC6RLG","0JR5R5BJG","08VW66RLF"]
 
     for user_id in user_ids:
 
         api = API(user_id)
         info = api.get_user_info(user_id)
         try:
+            pid = info['code']
             name = info['name']
+            print(name,pid)
         except:
-            print(name)
+            print(name+"failed")
 
-        path =  name + ".csv"
-        if (os.path.exists( name + ".csv") == False):
+        path =  pid + ".csv"
+        if (os.path.exists( pid + ".csv") == False):
             api.create_csv(user_id)
-        elif (os.path.exists( name + ".csv") ):
+        elif (os.path.exists( pid + ".csv") ):
             win, plays, rate, rate_change, win_total, plays_total, versus_rating = api.get_print(user_id)
             if(plays != -1):
                 api.write_to_csv(user_id)
